00b1885ec903be1d4135013b1031d122f51a810a
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-04-19 14:20:45 -0700

    added src and effective pom

---
+++ src/main/java/com/starbucks/kirk/App.java
@@ -0,0 +1,26 @@
+package com.starbucks.kirk;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Args size: " + args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+		
+		;
+		System.out.println(Joiner.on("--").join(Lists.newArrayList(4,5,3,5,10)));
+		
+		Sets.newHashSet(4,5,2,1,5,9,10);
+		System.out.println("Hello World!");
+		
+		
+	}
+}
